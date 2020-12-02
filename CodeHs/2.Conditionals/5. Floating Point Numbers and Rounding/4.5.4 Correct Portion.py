d4d1dcb3488258cebdeb9de05ae70e2e554cde9a
NathanTHEhcaker <75396388+NathanTHEhcaker@users.noreply.github.com>
2020-12-02 15:55:28 -0600

    Update 4.5.4 Correct Portion.py
    hey so you just need to add the paranthesis on the stuff u want to print

--- CodeHs/2.Conditionals/5. Floating Point Numbers and Rounding/4.5.4 Correct Portion.py
+++ CodeHs/2.Conditionals/5. Floating Point Numbers and Rounding/4.5.4 Correct Portion.py
@@ -5,7 +5,7 @@ num_people = 25
 # Determine how much food each person gets
 tons_of_food_per_person = tons_of_food / num_people
 
-print tons_of_food_per_person
+print (tons_of_food_per_person)
 
 # Ask the user how much food they took
 
@@ -14,6 +14,6 @@ round(tons_of_food_per_person, 5)
 round(tons_taken, 5)
 
 if tons_taken == tons_of_food_per_person:
-    print "Good job, you took the right amount of food!"
+    print ("Good job, you took the right amount of food!")
 else:
-    print "You took the wrong amount of food!"
+    print ("You took the wrong amount of food!")
